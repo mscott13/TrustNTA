32512fb1494ee00b107368b9fc554fe634f77098
unknown <a.markscott13@gmail.com>
2020-03-25 17:23:59 -0500

    update

---
+++ build/TrustNTA/Scripts/site.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollPos = $(this).scrollTop();
+        var elementHeight = $(this).height();
+        var result = 1 - (elementHeight - scrollPos) / elementHeight;
+        $('.main-nav').css('background-color', 'rgba(255,255,255,0.2) !important');
+    });
+});
